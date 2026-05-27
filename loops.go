205d0c641e4bf493580f3c7338d9233f8f489932
gustavomonks <gustavo.henrique@monks.com>
2026-05-27 09:45:52 -0300

    types of for loops

---
+++ loops.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	for n := range 6 {
+		// continue to the next iteration of the loop
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n + 1)
+	}
+
+}
