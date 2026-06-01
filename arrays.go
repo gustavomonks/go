50f382fdd6f0a71692a331d424be0b8bcc55d1f7
gustavomonks <gustavo.henrique@monks.com>
2026-06-01 10:18:30 -0300

    explore slice operations

--- arrays.go
+++ arrays.go
@@ -16,6 +16,18 @@ func main() {
 		{1, 2, 3},
 		{3, 2, 1},
 	}
-
 	fmt.Println(c)
+
+	var s = make([]string, 3)
+	fmt.Println(s)
+	// append only works when we use slice, because arrays have a fixed sized
+	s = append(s, "a")
+	fmt.Println(s)
+	fmt.Println(len(s))
+	s[1] = "b"
+	s[0] = "c"
+	fmt.Println(s)
+	fmt.Println(s[1])
+	fmt.Println(s[3:4]) // create a slice starting 3 and end before 4
+
 }
