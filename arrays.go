24f0ae0ea09d79aa9eb8b3fbbb165e9e1dc42da8
gustavomonks <gustavo.henrique@monks.com>
2026-06-01 09:50:13 -0300

    basic examples how we create arrays

--- arrays.go
+++ arrays.go
@@ -1,5 +1,21 @@
 package main
 
-func arrays() {
+import "fmt"
 
+func main() {
+	var a [3]int
+	fmt.Println(a)
+	fmt.Println(len(a))
+	a[len(a)-1] = 2
+	fmt.Println(a)
+
+	b := [...]int{1, 2, 3}
+	fmt.Println(b)
+
+	var c = [2][3]int{
+		{1, 2, 3},
+		{3, 2, 1},
+	}
+
+	fmt.Println(c)
 }
