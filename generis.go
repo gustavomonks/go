a8ecf6e8d5fcbb6a7c94159ee5d6e8ca35c3e855
gustavomonks <gustavo.henrique@monks.com>
2026-06-22 13:54:22 -0300

    introduce generics

---
+++ generis.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+func AddInt(a int, b int) int {
+	return a + b
+}
+
+func AddFloat(a float64, b float64) float64 {
+	return a + b
+}
+
+func Add[T int | float64](a T, b T) T {
+	return a + b
+}
+
+// -----
+
+type CustomData interface {
+	constraints.Ordered | []byte | []rune
+}
+
+type UserGenerics[T CustomData] struct {
+	ID   int
+	Name string
+	Data T
+}
+
+func main() {
+
+	fmt.Println(AddFloat(2, 4.5))
+	fmt.Println(AddInt(2, 4))
+	//fmt.Println(AddInt(2.23, 1)) //wont work
+	fmt.Println(Add(2.523, 1))
+	// here we passa what type we want for Data
+	u := UserGenerics[int]{
+		ID:   2,
+		Name: "Gustavo",
+		Data: 19,
+	}
+	fmt.Println(u)
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example/hello
 
 go 1.26.3
+
+require golang.org/x/exp v0.0.0-20260611194520-c48552f49976
